9283bbd2a52406fef21c0b5930d08e485f48a20a
Trifon Kovalenko <trifonkovalenko409@gmail.com>
2025-06-16 17:27:05 +0300

    16/06/2025

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -3,6 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Все URL блога начинаются с /blog/ и берутся из blog/urls.py
     path('blog/', include('blog.urls', namespace='blog')),
 ]
