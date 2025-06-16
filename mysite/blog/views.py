3cf49a53a349bade6ef1bf3e3931729ec56910fe
Trifon Kovalenko <trifonkovalenko409@gmail.com>
2025-06-16 18:24:44 +0300

    16/06/2025

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,11 +3,15 @@ from .models import Post
 from django.core.paginator import Paginator
 
 def post_list(request):
-    posts = Post.published.all()  # Использует фильтр по статусу PUBLISHED
+    post_list = Post.published.all()
     # Постраничная разбивка с 3 постами на страницу
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    posts = paginator.page(page_number)
+    return render(request,
+    'blog/post/list.html',
+    {'posts': posts})
+
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
