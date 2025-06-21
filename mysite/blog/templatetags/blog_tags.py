45417d4697e1c00ffd2fed9b074eb47f598885e8
Trifon Kovalenko <trifonkovalenko409@gmail.com>
2025-06-21 15:57:45 +0300

    20/06/2025

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -1,12 +1,7 @@
-from multiprocessing.resource_tracker import register
-
 from django import template
-from django.template.autoreload import template_changed
-
 from ..models import Post
 
 register = template.Library()
-
 @register.simple_tag
 def total_posts():
     return Post.published.count()
