129e4f76fa964585d123d4d864c9150abc4ad49d
Trifon Kovalenko <trifonkovalenko409@gmail.com>
2025-06-28 23:18:51 +0300

    22/06/2025

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -10,6 +10,27 @@ from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
 
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+                search=SearchVector('title', 'body'),
+            ).filter(search=query)
+
+        return render(request,
+                      'blog/post/search.html',
+                      {'form': form,
+                       'query': query,
+                       'results': results})
+
+
+
 
 def post_share(request, post_id):
     # Извлечь пост по его идентификатору id
