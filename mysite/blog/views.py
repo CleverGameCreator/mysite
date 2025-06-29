31f9cdf3f5626ede4dd9f7692ab6161e46419114
Trifon Kovalenko <trifonkovalenko409@gmail.com>
2025-06-29 19:20:06 +0300

    22/06/2025

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -11,6 +11,8 @@ from taggit.models import Tag
 from django.db.models import Count
 from django.contrib.postgres.search import SearchVector, \
                                            SearchQuery, SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
+
 
 def post_search(request):
     form = SearchForm()
@@ -25,9 +27,8 @@ def post_search(request):
                             SearchVector('body', weight='B')
             search_query = SearchQuery(query)
             results = Post.published.annotate(
-                search=search_vector,
-                rank=SearchRank(search_vector, search_query)
-            ).filter(rank__gte=0.3).order_by('-rank')
+                similarity=TrigramSimilarity('title', query),
+            ).filter(similarity__gt=0.1).order_by('-similarity')
 
         return render(request,
                       'blog/post/search.html',
